485ca1d74157cfe13c7627d2c61f429713ff7ed9
AshMikhail <151661993+AshMikhail@users.noreply.github.com>
2024-06-24 18:36:21 +0300

    Update Задание 1.js

--- Задание 1.js
+++ Задание 1.js
@@ -14,7 +14,7 @@ let zero = 0,
 
 for (let i = 0; i < array.length; i++) {
     if (typeof array[i] === "number" && !isNaN(array[i])) {
-        if (i === 0) {
+        if (array[i] === 0) {
             zero += 1;
         }else if (array[i] % 2 === 0) {
             even += 1;
